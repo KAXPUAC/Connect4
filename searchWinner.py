a8593ed8f169ab5adc9dcf28baf9734897c8b2f4
Kevin Axpuac <kevinjuarez@galileo.edu>
2020-05-31 06:31:35 -0600

    Se corrige comentario

--- searchWinner.py
+++ searchWinner.py
@@ -16,7 +16,7 @@ def searchWinner(board, symbol: int):
         for j in range(len(board)-3):
             if board[j][c] == symbol and board[j + 1][c + 1] == symbol and board[j + 2][c + 2] == symbol and board[j + 3][c + 3] == symbol:
                 return True
-    #Verifica Digonales invertida \
+    #Verifica Digonales normales /
     for c in range(columns - 3):
         for j in range(3,len(board)):
             if board[j][c] == symbol and board[j - 1][c + 1] == symbol and board[j - 2][c + 2] == symbol and board[j - 3][c + 3] == symbol:
