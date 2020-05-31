476299d31b683352011b7f2d5e762e7ab7e90b6f
Kevin Axpuac <kevinjuarez@galileo.edu>
2020-05-31 10:04:57 -0600

    Se corrigen validaciones de tamaño

--- sizeBoard.py
+++ sizeBoard.py
@@ -5,6 +5,10 @@ def sizeBoar(sizeT):
         sizeT.strip()
         uno = re.compile("[5-9]{1}\*[6-9]{1}")
         unno = re.compile("[5-9]{1}[x|X][6-9]{1}")
+        diez = re.compile("[5-9]{1}[x|X]10{1}")
+        dieez = re.compile("[5-9]{1}\*10{1}")
+        diezreverse = re.compile("10{1}[x|X][6-9]{1}")
+        dieezreverse = re.compile("10{1}\*[6-9]{1}")
         dos = re.compile("10\*10")
         doss = re.compile("10{1}[x|X]10{1}")
         if uno.fullmatch(sizeT):
@@ -28,6 +32,30 @@ def sizeBoar(sizeT):
             sizeT = sizeT.split("x")
             if len(sizeT) != 2:
                 sizeT = sizeT[0].split("X")
+            size[0] = int(sizeT[0])
+            size[1] = int(sizeT[1])
+            return size
+        elif diez.fullmatch(sizeT):
+            sizeT = sizeT.split("x")
+            if len(sizeT) != 2:
+                sizeT = sizeT[0].split("X")
+            size[0] = int(sizeT[0])
+            size[1] = int(sizeT[1])
+            return size
+        elif dieez.fullmatch(sizeT):
+            sizeT = sizeT.split("*")
+            size[0] = int(sizeT[0])
+            size[1] = int(sizeT[1])
+            return size
+        elif diezreverse.fullmatch(sizeT):
+            sizeT = sizeT.split("x")
+            if len(sizeT) != 2:
+                sizeT = sizeT[0].split("X")
+            size[0] = int(sizeT[0])
+            size[1] = int(sizeT[1])
+            return size
+        elif dieezreverse.fullmatch(sizeT):
+            sizeT = sizeT.split("*")
             size[0] = int(sizeT[0])
             size[1] = int(sizeT[1])
             return size
