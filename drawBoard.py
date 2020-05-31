036ea205a02868d35f1dad5ec0433c703b9baecd
Kevin Axpuac <kevinjuarez@galileo.edu>
2020-05-31 04:25:56 -0600

    Se modifica la forma en que se dibuja el tablero

--- drawBoard.py
+++ drawBoard.py
@@ -1,24 +1,48 @@
-def drawBoard(col: int, fil: int, tabla):
-    from colorama import init, Fore, Back, Style
+from colors import Colors
+def drawBoard(board):
     head = ''
-    final = ''
-    for x in range(col):
+    footer = ''
+    for x in range(0, len(board[0])):
         head = head + " " + str(x + 1)
-    for i in range(fil):
-        tablero = ''
-        for j in range(col):
-            tablero = tablero[0:len(tablero) - 1] + "|"
-            if tabla[i][j] == 0:
-                tablero += Fore.RED + "O"
-            elif tabla[i][j] == 1:
-                tablero += Fore.GREEN + "O"
+        footer += "+-"
+    print(head)
+    count = 0
+    for x in range(0,len(board)):
+        row = "|"
+        for i in range(0,len(board[x])):
+            if board[x][i] is None:
+                count += 1
+                row += " "
+            elif board[x][i] == 0:
+                row += Colors.CRED + "O" + Colors.CWHITE
             else:
-                tablero += " "
-            tablero += Fore.WHITE + "|"
-        if i == 0:
-            print(head)
-        print(tablero)
-    for x in range(0, col):
-        final = final + "+-"
-    final = final + "+"
-    print(final)
+                row += Colors.CBLUE + "O" + Colors.CWHITE
+            row += "|"
+        print(row)
+    footer += "+"
+    print(footer)
+    print("Tiros faltantes",count)
+# def drawBoard(col: int, fil: int, tabla):
+#     from colorama import init, Fore, Back, Style
+#     head = ''
+#     final = ''
+#     for x in range(col):
+#         head = head + " " + str(x + 1)
+#     for i in range(fil):
+#         tablero = ''
+#         for j in range(col):
+#             tablero = tablero[0:len(tablero) - 1] + "|"
+#             if tabla[i][j] == 0:
+#                 tablero += Fore.RED + "O"
+#             elif tabla[i][j] == 1:
+#                 tablero += Fore.GREEN + "O"
+#             else:
+#                 tablero += " "
+#             tablero += Fore.WHITE + "|"
+#         if i == 0:
+#             print(head)
+#         print(tablero)
+#     for x in range(0, col):
+#         final = final + "+-"
+#     final = final + "+"
+#     print(final)
