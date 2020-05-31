1924ad3ce8cf2b519592857b024d1f36e432292a
Kevin Axpuac <kevinjuarez@galileo.edu>
2020-05-31 06:25:56 -0600

    Se completa el buscar al ganador

--- searchWinner.py
+++ searchWinner.py
@@ -11,5 +11,15 @@ def searchWinner(board, symbol: int):
         for j in range(len(board)-3):
             if board[j][c] == symbol and board[j + 1][c] == symbol and board[j + 2][c] == symbol and board[j + 3][c] == symbol:
                 return True
-    #Verifica Digonales
+    #Verifica Digonales invertida \
+    for c in range(columns - 3):
+        for j in range(len(board)-3):
+            if board[j][c] == symbol and board[j + 1][c + 1] == symbol and board[j + 2][c + 2] == symbol and board[j + 3][c + 3] == symbol:
+                return True
+    #Verifica Digonales invertida \
+    for c in range(columns - 3):
+        for j in range(3,len(board)):
+            if board[j][c] == symbol and board[j - 1][c + 1] == symbol and board[j - 2][c + 2] == symbol and board[j - 3][c + 3] == symbol:
+                return True
+
     return winner
